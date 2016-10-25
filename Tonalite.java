2a04964a12d03cc081962096f02ad7f76cc27d73
dvAf34 <elisabeth_vas@hotmail.com>
2016-10-25 08:43:01 +0200

    Add files via upload
    commit du 25/10

---
+++ Tonalite.java
@@ -0,0 +1,30 @@
+package robot.org.boiteameuh;
+
+/**
+ * Created by 34011-73-04 on 20/10/2016.
+ */
+
+public class Tonalite  {
+
+
+    public String nomTon;
+
+    public Tonalite(String nom){
+        this.nomTon=nom;
+    }
+
+    public String getNomTon() {
+        return nomTon;
+    }
+
+    public void setNomTon(String nomTon) {
+        this.nomTon = nomTon;
+    }
+
+
+
+
+
+
+
+}
